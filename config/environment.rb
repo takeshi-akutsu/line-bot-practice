14811c419700b3394ecd689edadb753a1c1904f9
阿久津岳志 <sky_10577@yahoo.co.jp>
2017-01-26 15:45:28 +0900

    zissou

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# line_clientを追加
+require 'line_client'
+
 # Initialize the Rails application.
 Rails.application.initialize!
